28b3ae3898326c6ad67c9d276881530855c80f7d
vixyninja <hhvy2003.dev@gmail.com>
2025-11-08 19:38:52 +0700

    feat(server): enhance Server struct with additional timeout configurations and middleware options for improved request handling

--- chi/chi.go
+++ chi/chi.go
@@ -2,16 +2,13 @@ package chi
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"net/http"
 	"os"
-	"os/signal"
 	"path/filepath"
 	"reflect"
 	"runtime"
 	"strings"
-	"syscall"
 	"text/tabwriter"
 	"time"
 
@@ -23,39 +20,104 @@ import (
 )
 
 type Server struct {
-	router      *chi.Mux
-	httpServer  *http.Server
-	log         logx.Logx
-	addr        string
-	stopTimeout time.Duration
-	moduleName  string
-	printRoutes bool
+	router            *chi.Mux
+	httpServer        *http.Server
+	log               logx.Logx
+	addr              string
+	stopTimeout       time.Duration
+	moduleName        string
+	printRoutes       bool
+	readHeaderTimeout time.Duration
+	readTimeout       time.Duration
+	writeTimeout      time.Duration
+	idleTimeout       time.Duration
+	requestTimeout    time.Duration
+	enableRequestID   bool
+	enableRealIP      bool
+	enableRecoverer   bool
 }
 
 type Option func(*Server)
 
-func WithAddr(addr string) Option            { return func(s *Server) { s.addr = addr } }
-func WithLogger(l logx.Logx) Option          { return func(s *Server) { s.log = l } }
-func WithStopTimeout(d time.Duration) Option { return func(s *Server) { s.stopTimeout = d } }
+func WithAddr(addr string) Option {
+	return func(s *Server) { s.addr = addr }
+}
+
+func WithLogger(l logx.Logx) Option {
+	return func(s *Server) { s.log = l }
+}
+
+func WithStopTimeout(d time.Duration) Option {
+	return func(s *Server) { s.stopTimeout = d }
+}
+
 func WithPrintRoutes(enabled bool) Option {
 	return func(s *Server) { s.printRoutes = enabled }
 }
+
+func WithReadHeaderTimeout(d time.Duration) Option {
+	return func(s *Server) { s.readHeaderTimeout = d }
+}
+
+func WithReadTimeout(d time.Duration) Option {
+	return func(s *Server) { s.readTimeout = d }
+}
+
+func WithWriteTimeout(d time.Duration) Option {
+	return func(s *Server) { s.writeTimeout = d }
+}
+
+func WithIdleTimeout(d time.Duration) Option {
+	return func(s *Server) { s.idleTimeout = d }
+}
+
+func WithRequestTimeout(d time.Duration) Option {
+	return func(s *Server) { s.requestTimeout = d }
+}
+
+func WithRequestID(enabled bool) Option {
+	return func(s *Server) { s.enableRequestID = enabled }
+}
+
+func WithRealIP(enabled bool) Option {
+	return func(s *Server) { s.enableRealIP = enabled }
+}
+
+func WithRecoverer(enabled bool) Option {
+	return func(s *Server) { s.enableRecoverer = enabled }
+}
 func NewServer(opts ...Option) *Server {
 	s := &Server{
-		router:      chi.NewRouter(),
-		log:         logx.NewStdLogger(),
-		addr:        ":4433",
-		stopTimeout: 10 * time.Second,
-		moduleName:  getModuleName(),
+		router:            chi.NewRouter(),
+		log:               logx.NewStdLogger(),
+		addr:              ":4433",
+		stopTimeout:       10 * time.Second,
+		moduleName:        getModuleName(),
+		readHeaderTimeout: 30 * time.Second,
+		readTimeout:       60 * time.Second,
+		writeTimeout:      60 * time.Second,
+		idleTimeout:       90 * time.Second,
+		requestTimeout:    60 * time.Second,
+		enableRequestID:   true,
+		enableRealIP:      true,
+		enableRecoverer:   true,
 	}
-
-	s.router.Use(middleware.RequestID)
-	s.router.Use(middleware.RealIP)
-	s.router.Use(middleware.Recoverer)
-	s.router.Use(middleware.Timeout(60 * time.Second))
 
 	for _, opt := range opts {
 		opt(s)
+	}
+
+	if s.enableRequestID {
+		s.router.Use(middleware.RequestID)
+	}
+	if s.enableRealIP {
+		s.router.Use(middleware.RealIP)
+	}
+	if s.enableRecoverer {
+		s.router.Use(middleware.Recoverer)
+	}
+	if s.requestTimeout > 0 {
+		s.router.Use(middleware.Timeout(s.requestTimeout))
 	}
 
 	return s
@@ -63,46 +125,25 @@ func NewServer(opts ...Option) *Server {
 
 func (s *Server) Router() *chi.Mux { return s.router }
 
-func (s *Server) Run() error {
-	return s.RunContext(context.Background())
+func (s *Server) HTTPServer() *http.Server {
+	if s.httpServer == nil {
+		s.httpServer = &http.Server{
+			Addr:              s.addr,
+			Handler:           s.router,
+			ReadHeaderTimeout: s.readHeaderTimeout,
+			ReadTimeout:       s.readTimeout,
+			WriteTimeout:      s.writeTimeout,
+			IdleTimeout:       s.idleTimeout,
+		}
+	}
+	return s.httpServer
 }
 
-func (s *Server) RunContext(ctx context.Context) error {
+func (s *Server) Run() error {
 	if s.printRoutes {
 		s.PrintRoutes()
 	}
-	s.httpServer = &http.Server{
-		Addr:              s.addr,
-		Handler:           s.router,
-		ReadHeaderTimeout: 30 * time.Second,
-		ReadTimeout:       60 * time.Second,
-		WriteTimeout:      60 * time.Second,
-		IdleTimeout:       90 * time.Second,
-	}
-
-	// TODO: using hook
-	ctx, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-
-	go func() {
-		<-ctx.Done()
-		shutdownCtx, cancel := context.WithTimeout(context.Background(), s.stopTimeout)
-		defer cancel()
-		if err := s.httpServer.Shutdown(shutdownCtx); err != nil {
-			s.log.Info(context.TODO(), fmt.Sprintf("graceful shutdown failed: %v", err))
-		}
-	}()
-
-	s.log.Info(context.TODO(), fmt.Sprintf("HTTP server listening on %s", s.addr))
-
-	err := s.httpServer.ListenAndServe()
-	if err != nil && !errors.Is(err, http.ErrServerClosed) {
-		s.log.Info(context.TODO(), fmt.Sprintf("listen and serve error: %v", err))
-		return err
-	}
-
-	s.log.Info(context.TODO(), "server stopped")
-	return nil
+	return s.httpServer.ListenAndServe()
 }
 
 func (s *Server) PrintRoutes(excludePatterns ...string) {
